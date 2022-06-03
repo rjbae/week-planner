3b01718def3c59b35ea12cd0faf3fadbf24c3f41
Andre Le <logisticx@yahoo.com>
2022-06-03 21:50:33 +0000

    main.js functions

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var $addEntryButton = document.querySelector('.add-entry-button');
 var $modalContainer = document.querySelector('.overlay-modal-container');
 var $submitButton = document.querySelector('.submit-button');
+var $form = document.querySelectorAll('form');
+var $selectDay = document.getElementById('day');
+var $selectTime = document.getElementById('time');
+var $textArea = document.getElementById('text-area');
+
+$form.addEventListener('submit', formSubmitEvent);
 
 $addEntryButton.addEventListener('click', openModal);
 $submitButton.addEventListener('click', closeModal);
@@ -9,6 +15,11 @@ function openModal(event) {
   $modalContainer.classList.remove('hidden');
 }
 
-function closeModal(event) {
+function formSubmitEvent(event) {
   $modalContainer.classList.add('hidden');
+  var newObj = {
+    time: $selectTime.value,
+    day: $selectDay.value
+  };
+
 }
